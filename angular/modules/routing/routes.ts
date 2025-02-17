5f043246560639e1c11c30848628091ff1616f75
theoliverlear <sigwartholiver2@gmail.com>
2025-02-17 06:53:42 -0600

    Add homeRoute with meta title "Anxiety Alert" and integrate it into routes.ts

--- angular/modules/routing/routes.ts
+++ angular/modules/routing/routes.ts
@@ -1,5 +1,16 @@
 import {Route} from "@angular/router";
+import {HomeComponent} from "../../components/pages/home/home.component";
+
+export const homeRoute: Route = {
+    path: "",
+    component: HomeComponent,
+    data: {
+        meta: {
+            title: "Anxiety Alert"
+        }
+    }
+};
 
 export const routes: Route[] = [
-
+    homeRoute
 ];
