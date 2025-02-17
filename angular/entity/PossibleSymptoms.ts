03be088caba557b968d07ff06bd4995ca394b8aa
theoliverlear <sigwartholiver2@gmail.com>
2025-02-17 07:11:29 -0600

    Add PossibleSymptoms.ts to define and export a list of potential symptom entities for the Angular application

---
+++ angular/entity/PossibleSymptoms.ts
@@ -0,0 +1,33 @@
+import {Symptom} from "./Symptom.entity";
+
+
+const tightChestSymptom = new Symptom("Tight Chest", "Chest feels normal.", "Chest feels tight.");
+const headSensationsSymptom = new Symptom("Head Sensations", "Clear, not heavy.", "Headache, feels full.");
+const blankMindSymptom = new Symptom("Blank Mind", "Normal flow of thoughts.", "Mind is blank, nothing there.");
+const thoughtsSymptom = new Symptom("Thoughts",
+                             "No negative, depressive thoughts. Able to think about things going on.",
+                             "Rapid, stressful thoughts. Negative self-talk. Low self-esteem.");
+const bodySensationsSymptom = new Symptom("Body Sensations", "Awake and aware.", "Tired, exhausted, numb.");
+const feelingsSymptom = new Symptom("Feelings", "Happy, calm, content.", "Sadness, anger, frustration, hopelessness");
+const behaviorsSymptom = new Symptom("Behaviors",
+    "Not avoidant of activities. No urge to isolate. No negative self talk.",
+    "Lazy, anhedonia, negative self-talk, avoidance, attention span decreased.");
+const possibleSymptoms: Symptom[] = [
+    tightChestSymptom,
+    headSensationsSymptom,
+    blankMindSymptom,
+    thoughtsSymptom,
+    bodySensationsSymptom,
+    feelingsSymptom,
+    behaviorsSymptom
+];
+export {
+    possibleSymptoms,
+    tightChestSymptom,
+    headSensationsSymptom,
+    blankMindSymptom,
+    thoughtsSymptom,
+    bodySensationsSymptom,
+    feelingsSymptom,
+    behaviorsSymptom
+}
