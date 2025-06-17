d18fb34fee358c75d54be012a14c3d57b90cbf82
theoliverlear <sigwartholiver2@gmail.com>
2025-06-17 10:37:38 -0500

    Add new "making sense" symptom to more accurately determine attacks in PossibleSymptoms.ts

--- angular/entity/PossibleSymptoms.ts
+++ angular/entity/PossibleSymptoms.ts
@@ -2,11 +2,14 @@ import {Symptom} from "./Symptom.entity";
 
 
 const tightChestSymptom = new Symptom("Tight Chest", "Chest feels normal.", "Chest feels tight.");
+const notMakingSense = new Symptom("Not Making Sense",
+    "Thoughts are clearly communicated, no confusion.",
+    "Thoughts are jumbled, not making sense, confused.");
 const headSensationsSymptom = new Symptom("Head Sensations", "Clear, not heavy.", "Headache, feels full.");
 const blankMindSymptom = new Symptom("Blank Mind", "Normal flow of thoughts.", "Mind is blank, nothing there.");
 const thoughtsSymptom = new Symptom("Thoughts",
-                             "No negative, depressive thoughts. Able to think about things going on.",
-                             "Rapid, stressful thoughts. Negative self-talk. Low self-esteem.");
+    "No negative, depressive thoughts. Able to think about things going on.",
+    "Rapid, stressful thoughts. Negative self-talk. Low self-esteem.");
 const bodySensationsSymptom = new Symptom("Body Sensations", "Awake and aware.", "Tired, exhausted, numb.");
 const feelingsSymptom = new Symptom("Feelings", "Happy, calm, content.", "Sadness, anger, frustration, hopelessness");
 const behaviorsSymptom = new Symptom("Behaviors",
@@ -14,6 +17,7 @@ const behaviorsSymptom = new Symptom("Behaviors",
     "Lazy, anhedonia, negative self-talk, avoidance, attention span decreased.");
 const possibleSymptoms: Symptom[] = [
     tightChestSymptom,
+    notMakingSense,
     headSensationsSymptom,
     blankMindSymptom,
     thoughtsSymptom,
@@ -23,6 +27,7 @@ const possibleSymptoms: Symptom[] = [
 ];
 export {
     possibleSymptoms,
+    notMakingSense,
     tightChestSymptom,
     headSensationsSymptom,
     blankMindSymptom,
